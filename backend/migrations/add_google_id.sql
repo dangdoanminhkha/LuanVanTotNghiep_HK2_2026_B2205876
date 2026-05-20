f0ee746bc52dc5e13153db332eff9df621d2368e
dangdoanminhkha <minhkha05876@gmail.com>
2026-05-20 13:58:39 +0700

    Xoá file thừa

--- backend/migrations/add_google_id.sql
+++
@@ -1,8 +0,0 @@
--- Add Google OAuth columns to users table
-ALTER TABLE users ADD COLUMN google_id VARCHAR(255) UNIQUE DEFAULT NULL;
-ALTER TABLE users ADD COLUMN auth_provider VARCHAR(50) DEFAULT 'email'; -- 'email', 'google', 'facebook', etc.
-ALTER TABLE users ADD COLUMN avatar_url TEXT DEFAULT NULL;
-
--- Create index for faster google_id lookups
-CREATE INDEX idx_google_id ON users(google_id);
-CREATE INDEX idx_auth_provider ON users(auth_provider);
